a15cec1d96cdfa2a9f4870856a3227fda2cf8fbb
Mohim Khan <mohimkhan652@gmail.com>
2023-07-16 00:31:25 +0600

    code push:adding delay on search

--- js/preloader.js
+++ js/preloader.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const images = document.querySelectorAll(".country-image img");
 
 window.addEventListener('load', () => {
   container.classList.add('hidden');
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+// import { resolve } from 'path';
 
 export default defineConfig({
     base: '/',
@@ -9,7 +10,33 @@ export default defineConfig({
                 index: './index.html',
                 country: './pages/country.html',
                 // Add more HTML files here if needed
+                // main: (path.resolve(__dirname, '.js/'))
+                main: './**js/*.js'
+            },
+            output: {
+                format: 'cjs', // Transpile code as CommonJS
             },
         },
     },
 });
+
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, './js/*.js'),
+//       },
+//     },
+//   },
+//   css: {
+//     postcss: {
+//       plugins: [],
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       '@': resolve(__dirname, './js'),
+//     },
+//   },
+// });
