b0ce35a70e496ffaddcf9dd09b35548e3f42b815
Mohim Khan <mohimkhan652@gmail.com>
2023-09-15 21:23:18 +0600

    move to src folder

--- js/preloader.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector('.container');
-const images = document.querySelectorAll(".country-image img");
-
-window.addEventListener('load', () => {
-  container.classList.add('hidden');
-});
