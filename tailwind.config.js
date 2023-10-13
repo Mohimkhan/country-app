d6dfb93d65243c68a83296c32cf42ae24a19baf8
Mohim Khan <mohimkhan652@gmail.com>
2023-10-13 18:36:00 +0600

    added flip animation with tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,30 @@ module.exports = {
         "dark-gray-400": "var(--very-light-gray)",
         "white-700": "var(--white)",
       },
+      keyframes: {
+        flip: {
+          '0%': {
+            transform: 'perspective(400px) rotateY(0)',
+            'animation-timing-function': 'ease-out',
+          },
+          '40%': {
+            transform: 'perspective(400px) translateZ(50px) rotateY(170deg)',
+            'animation-timing-function': 'ease-out',
+          },
+          '50%': {
+            transform: 'perspective(400px) translateZ(80px) rotateY(190deg) scale(1)',
+            'animation-timing-function': 'ease-in',
+          },
+          '80%': {
+            transform: 'perspective(400px) rotateY(360deg) scale(0.95)',
+            'animation-timing-function': 'ease-in',
+          },
+          '100%': {
+            transform: 'perspective(400px) scale(1)',
+            'animation-timing-function': 'ease-in',
+          },
+        },
+      },
     },
   },
   plugins: [],
